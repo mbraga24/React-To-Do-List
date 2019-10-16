a80d64b3ffad9c6163c93c662c0946a6201fcfe7
Marlon Braga <rdg.brg@gmail.com>
2019-10-16 14:17:15 -0400

    Updates

---
+++ precache-manifest.e26ddaed7b543c5e408856b76895bc58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38521bbd494b80d9f8ae61f68dcf8ac",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "8a3249dd8e0670687dd2",
+    "url": "/React-ToDo/static/css/main.e92f66a6.chunk.css"
+  },
+  {
+    "revision": "80fd8c2b025449ab0aa3",
+    "url": "/React-ToDo/static/js/2.7a3df354.chunk.js"
+  },
+  {
+    "revision": "8a3249dd8e0670687dd2",
+    "url": "/React-ToDo/static/js/main.50344649.chunk.js"
+  },
+  {
+    "revision": "83ddc53d1b465b11b2cd",
+    "url": "/React-ToDo/static/js/runtime-main.b8ba98f8.js"
+  }
+]);
